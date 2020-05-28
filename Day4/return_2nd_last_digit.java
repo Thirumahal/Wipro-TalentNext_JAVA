a1476875d5d6ff3c74fa914bd268ae35a2eb8f98
Thirumahal <64912582+Thirumahal@users.noreply.github.com>
2020-05-28 22:23:53 +0530

    Create return_2nd_last_digit.java

---
+++ Day4/return_2nd_last_digit.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*:
+class userMainCode
+{
+  public int secondLastDigitOf(int input){
+  //Read only region end
+  int n,sl,secondlast;
+  n=Math.abs(input);
+  if(n>9){
+    sl=n%100;
+    secondlast=sl/10;
+  }
+  return(secondlast);
+ }
+ }
