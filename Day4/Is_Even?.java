706102469157abde7aae3b3a84c1258ad5607dd6
Thirumahal <64912582+Thirumahal@users.noreply.github.com>
2020-05-28 22:32:30 +0530

    Update and rename Even?.java to Is_Even?.java

--- Day4/Is_Even?.java
+++ Day4/Is_Even?.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*:
 class userMainCode
 {
-  public int isOdd(int input){
+  public int isEven(int input){
   //Read only region end
   if(input%2==0){
       return 2;
