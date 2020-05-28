388c625beefa99d1d283ecbeef4a9c1b5972a80e
Thirumahal <64912582+Thirumahal@users.noreply.github.com>
2020-05-28 22:18:58 +0530

    Create return_last_digit.java

---
+++ Day4/return_last_digit.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*:
+class userMainCode
+{
+  public int lastdigit(int input){
+  //Read only region end
+  int lastdigit;
+  lastdigit=Math.abs(input%10);
+  return(lastdigit);
+ }
+ }
