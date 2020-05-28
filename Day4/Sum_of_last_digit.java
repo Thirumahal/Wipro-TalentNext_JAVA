78440da0eb588860241236d0b183a859da5ee830
Thirumahal <64912582+Thirumahal@users.noreply.github.com>
2020-05-28 22:31:12 +0530

    Create Sum_of_last_digit.java

---
+++ Day4/Sum_of_last_digit.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*:
+class userMainCode
+{
+  public int addlastdigits(int input1,int input2){
+  //Read only region end
+  int n,m,sum;
+  n=Math.abs(input1%10);
+  m=Math.abs((input2%10);
+  sum=n+m;
+  return(sum);
+ }
+ }
