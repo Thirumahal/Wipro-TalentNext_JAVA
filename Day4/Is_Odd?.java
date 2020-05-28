a257b0c66c0933e731861626d5d6a09471c0d506
Thirumahal <64912582+Thirumahal@users.noreply.github.com>
2020-05-28 22:15:16 +0530

    Update Is_Odd?.java

--- Day4/Is_Odd?.java
+++ Day4/Is_Odd?.java
@@ -1,1 +1,14 @@
-import
+import java.io.*;
+import java.util.*:
+class userMainCode
+{
+  public int isOdd(int input){
+  //Read only region end
+  if(input%2==0){
+      return 1;
+    }
+    else{
+      return 2;
+    }
+ }
+ }
